ba738b0611c2b77b0e0797faaa9f4666bf82847d
Fl4m3Z <145007012+0xFZs3c@users.noreply.github.com>
2026-03-06 13:36:28 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+function updateAge() {
+    const birthDate = new Date(2010, 2, 18);
+    const now = new Date();
+    const ageMs = now - birthDate;
+    const ageYears = ageMs / (1000 * 60 * 60 * 24 * 365.25);
 
+    const button = document.getElementById('ageButton');
+    if (button) {
+        button.textContent = `You just turned: ${ageYears.toFixed(12)} yo!`;
+    }
+}
+
+window.addEventListener('load', function () {
+    updateAge();
+    setInterval(updateAge, 1000);
+});
